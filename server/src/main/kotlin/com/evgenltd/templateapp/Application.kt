dd74f882531e8e8735fb0ca5b82fa9781f1d4019
Evgeniy Lebedev <lebedev1537@gmail.com>
2022-08-23 02:20:04 +0400

    Initial commit

---
+++ server/src/main/kotlin/com/evgenltd/templateapp/Application.kt
@@ -0,0 +1,14 @@
+package com.evgenltd.templateapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+@SpringBootApplication
+class Application : SpringBootServletInitializer() {
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
