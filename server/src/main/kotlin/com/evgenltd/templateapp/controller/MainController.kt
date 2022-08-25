ce77827fd0f4f8eae964ab28aa2328d7a6f47f73
Evgeniy Lebedev <lebedev1537@gmail.com>
2022-08-25 23:05:59 +0400

    CI/CD updates

---
+++ server/src/main/kotlin/com/evgenltd/templateapp/controller/MainController.kt
@@ -0,0 +1,12 @@
+package com.evgenltd.templateapp.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MainController {
+
+    @GetMapping("/main")
+    fun main(): String = "Hello, world!"
+
+}
--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  message: string = "None"
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<string>('/main')
+      .subscribe(result => this.message = result)
+  }
 
 }
