4d4a75066e27cc069e11053f8dc90dee12931e55
Evgeniy Lebedev <lebedev1537@gmail.com>
2022-08-27 00:03:25 +0400

    RestTemplate interceptor

--- server/src/main/kotlin/com/evgenltd/templateapp/Application.kt
+++ server/src/main/kotlin/com/evgenltd/templateapp/Application.kt
@@ -1,10 +1,14 @@
 package com.evgenltd.templateapp
 
+import com.evgenltd.templateapp.common.util.RestLoggingInterceptor
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean
+import org.springframework.http.client.BufferingClientHttpRequestFactory
+import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.web.client.RestTemplate
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -19,6 +23,12 @@ class Application : SpringBootServletInitializer() {
                 .allowedOrigins("http://localhost:4200/")
         }
     }
+
+    @Bean
+    fun restTemplate(interceptor: RestLoggingInterceptor) = RestTemplate(BufferingClientHttpRequestFactory(
+        SimpleClientHttpRequestFactory()
+    ))
+        .also { it.interceptors = it.interceptors + listOf(interceptor) }
 
 }
 
